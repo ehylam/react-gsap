0f431f510f0675f7d6b60316cdd3f1fce4a1c092
Eric Lam <ehylam.p@gmail.com>
2020-08-08 15:14:06 +1000

    gsap + react tut from ihatetomatoes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
+import { gsap } from 'gsap';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const headerRef = useRef(null);
+	const [background, setBackground] = useState('#fff');
+
+	const toggleBackground = () => {
+		const color = background !== '#fff' ? '#232323' : '#000';
+		setBackground(color);
+	};
+
+	useEffect(
+		() => {
+			gsap.to(headerRef.current, { duration: 1, backgroundColor: background, ease: 'none' });
+		},
+		[background]
+	);
+
+	useEffect(() => {
+		gsap.from(headerRef.current, {
+			duration: 1,
+			autoAlpha: 0,
+			ease: 'none',
+			delay: 1
+		});
+	}, []);
+	return (
+		<div className="App">
+			<header ref={headerRef} className="App-header">
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<button onClick={() => toggleBackground()}>Toggle Background</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
