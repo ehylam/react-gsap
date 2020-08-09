4f0d6c9256a2f376b2c9b4ecd3b7d734788fc6c7
Eric Lam <ehylam.p@gmail.com>
2020-08-09 13:30:43 +1000

    added ScrollTrigger animations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import './App.css';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = [
+	{
+		title: 'Title 1',
+		subtitle: 'Subtitle 1'
+	},
+	{
+		title: 'Title 2',
+		subtitle: 'Subtitle 2'
+	},
+	{
+		title: 'Title 3',
+		subtitle: 'Subtitle 3'
+	}
+];
 
 function App() {
 	const headerRef = useRef(null);
+	const revealRefs = useRef([]);
+	revealRefs.current = [];
 	const [background, setBackground] = useState('#fff');
 
 	const toggleBackground = () => {
@@ -25,15 +45,51 @@ function App() {
 			ease: 'none',
 			delay: 1
 		});
+
+		revealRefs.current.forEach((el, index) => {
+			gsap.fromTo(
+				el,
+				{
+					autoAlpha: 0
+				},
+				{
+					duration: 1,
+					autoAlpha: 1,
+					ease: 'none',
+					scrollTrigger: {
+						id: `section-${index + 1}`,
+						trigger: el,
+						start: 'top center+=100',
+						toggleActions: 'play none none reverse',
+						markers: true
+					}
+				}
+			);
+		});
 	}, []);
+
+	const addToRefs = el => {
+		if (el && !revealRefs.current.includes(el)) {
+			revealRefs.current.push(el);
+		}
+		console.log(el, revealRefs.current);
+	};
+
 	return (
 		<div className="App">
 			<header ref={headerRef} className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
 				<button onClick={() => toggleBackground()}>Toggle Background</button>
 			</header>
+			<main>
+				{sections.map(({ title, subtitle, index }) => {
+					return (
+						<div className="heading" key={title} ref={addToRefs}>
+							<h2>{title}</h2>
+							<h4>{subtitle}</h4>
+						</div>
+					);
+				})}
+			</main>
 		</div>
 	);
 }
